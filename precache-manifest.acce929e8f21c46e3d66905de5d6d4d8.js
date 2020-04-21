2a42219acb605b955edf66fb31f0a790c8a15b54
Samanta Martinez <msamanta@google.com>
2020-04-21 18:41:41 -0500

    initial commit

---
+++ precache-manifest.acce929e8f21c46e3d66905de5d6d4d8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29811dcbb1dc89e86ac66dc36a249017",
+    "url": "/index.html"
+  },
+  {
+    "revision": "781100977c1479d85e2b",
+    "url": "/static/css/main.111c5dfe.chunk.css"
+  },
+  {
+    "revision": "b0ab8ee158d695f0e594",
+    "url": "/static/js/2.7f1fa8bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7f1fa8bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "781100977c1479d85e2b",
+    "url": "/static/js/main.d4adebe5.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  },
+  {
+    "revision": "a973b7587003de54767c59d1f4e6a7dd",
+    "url": "/static/media/bg-large.a973b758.png"
+  },
+  {
+    "revision": "03430f3795bbb5989c57fe5692c756b1",
+    "url": "/static/media/bg-small.03430f37.png"
+  },
+  {
+    "revision": "9c3a6a082cad2730f107a1f7773c220c",
+    "url": "/static/media/facebook.9c3a6a08.svg"
+  },
+  {
+    "revision": "d86ffb96c0a0a2c9a8d405fec83fcace",
+    "url": "/static/media/fries.d86ffb96.svg"
+  },
+  {
+    "revision": "07318180b2834a0afc84d862d0a2c6a5",
+    "url": "/static/media/hamburger.07318180.svg"
+  },
+  {
+    "revision": "616a0a54916c7a6630cafebc0fb37855",
+    "url": "/static/media/ice-cream.616a0a54.svg"
+  },
+  {
+    "revision": "95c6178f196818b596467a92e618ef39",
+    "url": "/static/media/instagram.95c6178f.svg"
+  },
+  {
+    "revision": "ed0eaae7e8610ff57af27e39f14c818a",
+    "url": "/static/media/light-hamburger.ed0eaae7.svg"
+  },
+  {
+    "revision": "53aadabfa8d50f6c4b45db4641ae6d42",
+    "url": "/static/media/nav.53aadabf.svg"
+  },
+  {
+    "revision": "a6c5fb57ac4f8072431fb22f874a17b5",
+    "url": "/static/media/soda.a6c5fb57.svg"
+  },
+  {
+    "revision": "7dfde20a44e114353bb45152f67b7590",
+    "url": "/static/media/tiktok.7dfde20a.svg"
+  }
+]);
